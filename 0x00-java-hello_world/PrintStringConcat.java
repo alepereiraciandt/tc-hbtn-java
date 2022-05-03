222ab09b8931fcb70a6c4b1fa56ec2bf9dfcfb75
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-03 17:28:07 -0300

    fix: complaning about lines have 7 lines, 8 is needed

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,6 +1,7 @@
 public class PrintStringConcat {
     public static void main(String[] args) {
-        String texto1 = "Holberton", texto2 = "School";
+        String texto1 = "Holberton";
+        String texto2 = "School";
         String texto3 = texto1  + " " + texto2;
         System.out.printf("Bem vindo a %s!", texto3);
     }
