68911fb9ba99eb5e6824f2e9eb32744dff861926
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-03 19:05:16 -0300

    feat: PrintFormatacaoNumeros

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.text.NumberFormat;
+
+public class PrintFormatacaoNumeros {
+    public static void main(String[] args) {
+        float taxa = 0.2456f, valor = 7654.322f;
+
+        NumberFormat currency = NumberFormat.getInstance(new Locale("en", "US"));
+
+        currency.setMaximumFractionDigits(3);
+
+        System.out.println("Valor: R$ " + currency.format(valor));
+
+        System.out.printf("Taxa: %.2f%%", taxa);
+    }
+}
