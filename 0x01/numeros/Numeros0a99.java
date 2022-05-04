040d52f990f07eb4040f310d69399476a8c72514
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-04 16:22:51 -0300

    fix

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -3,7 +3,7 @@ package numeros;
 public class Numeros0a99 {
     public static void main(String[] args) {
         for (int i = 0; i <= 99; i += 1) {
-            System.out.printf(i + ", ");
+            System.out.printf(i == 99 ? " " : ", ");
         }
         System.out.printf("\n");
     }
