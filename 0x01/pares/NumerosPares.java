f52976667781a096e50a4b30585dbaaca2356156
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-04 17:18:52 -0300

    fix

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -2,7 +2,7 @@ public class NumerosPares {
     public static void main(String[] args) {
         for (int i = 0; i <= 99; i += 1) {
             if (i % 2 == 0) {
-                System.out.printf(i == 99 ? i + "" : i + ", ");
+                System.out.printf(i == 98 ? i + "" : i + ", ");
             }
         }
     }
