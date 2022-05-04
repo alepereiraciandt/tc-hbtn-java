d4896891ea0d77ba73e87d91444df41a9db77864
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-04 17:14:29 -0300

    feat: NumerosPares

---
+++ 0x01/numeros/NumerosPares.java
@@ -0,0 +1,9 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i += 1) {
+            if (i % 2 == 0) {
+                System.out.printf(i == 100 ? i + "" : i + ", ");
+            }
+        }
+    }
+}
