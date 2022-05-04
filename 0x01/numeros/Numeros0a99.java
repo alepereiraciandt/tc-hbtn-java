8cc84c910b8fb1bdce2cb34b0a3f45725a912705
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-04 16:15:31 -0300

    feat: Numeros0a99

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,9 @@
+package numeros;
+
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 99; i += 1) {
+            System.out.printf(i + ", ");
+        }
+    }
+}
