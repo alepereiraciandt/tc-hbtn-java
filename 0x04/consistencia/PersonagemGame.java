31c693002ef729f19512f28c4e99df02b6633154
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-12 14:20:35 -0300

    feat: 0x04 - consistencia

---
+++ 0x04/consistencia/PersonagemGame.java
@@ -0,0 +1,61 @@
+package consistencia;
+
+public class PersonagemGame {
+    private String nome;
+    private int saudeAtual;
+
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+       setSaudeAtual(saudeAtual);
+        setNome(nome);
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        this.saudeAtual -= quantidadeDeDano;
+        if (saudeAtual < 0) {
+            saudeAtual = 0;
+            setStatus("morto");
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        setStatus("vivo");
+        saudeAtual += quantidadeDeCura;
+        if (saudeAtual > 100) {
+            saudeAtual = 100;
+        }
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.isEmpty()) {
+            return;
+        }
+            this.nome = nome;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            setStatus("vivo");
+        } else {
+            setStatus("morto");
+        }
+    }
+}
