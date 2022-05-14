775e0c77c347fd82f0e5f49f94a220f72f007af6
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-14 09:24:39 -0300

    fix

--- 0x05/retangulo/FormaGeometrica.java
+++ 0x05/retangulo/FormaGeometrica.java
@@ -1,5 +1,3 @@
-package retangulo;
-
 public class FormaGeometrica {
     public double area() throws UnsupportedOperationException {
         throw new UnsupportedOperationException("Funcao area nao implementada");
