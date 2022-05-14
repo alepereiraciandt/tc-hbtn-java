89d7b038ba0fc3c4616857087884804d7da99a29
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-14 11:15:18 -0300

    feat: quadrado

---
+++ 0x05/quadrado/Quadrado.java
@@ -0,0 +1,24 @@
+package quadrado;
+
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado < 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        } else {
+            this.lado = lado;
+            this.setAltura(lado);
+            this.setLargura(lado);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] %1$.2f ", lado);
+    }
+}
