9c9ab0ffe27f955acfd0dfb9d4ac815604fdf1be
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-14 10:20:03 -0300

    feat: retangulo_completo

---
+++ 0x05/retangulo_completo/Retangulo.java
@@ -0,0 +1,41 @@
+package retangulo_completo;
+
+public class Retangulo extends FormaGeometrica {
+    private double largura;
+    private double altura;
+
+    public void setLargura(double largura) {
+        if (largura < 0) {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        } else {
+            this.largura = largura;
+        }
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        } else {
+            this.altura = altura;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Retangulo] " + largura + " / " + altura);
+    }
+
+    @Override
+    public double area() {
+        double areaDeUmTriangulo = (altura * largura);
+        return areaDeUmTriangulo;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+}
