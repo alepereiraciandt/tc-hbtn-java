5796a0863f4585a8a78c53df767fab3010b30567
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-14 08:58:41 -0300

    feat: forma

---
+++ 0x05/forma/FormaGeometrica.java
@@ -0,0 +1,7 @@
+package forma;
+
+public class FormaGeometrica {
+    public double area() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
