0e0f81565e0d100320f3acb58889e1d10d748183
alepereiraciandt <alexandrepereira@ciandt.com>
2022-05-14 09:00:06 -0300

    fix

--- 0x05/forma/FormaGeometrica.java
+++ 0x05/forma/FormaGeometrica.java
@@ -1,5 +1,3 @@
-package forma;
-
 public class FormaGeometrica {
     public double area() throws UnsupportedOperationException {
         throw new UnsupportedOperationException("Funcao area nao implementada");
