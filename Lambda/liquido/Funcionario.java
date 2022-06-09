b8a05d15358a32d606ac305a63eb00cf51cc51f6
alepereiraciandt <alexandrepereira@ciandt.com>
2022-06-09 19:11:06 -0300

    feat: salario funcionario

---
+++ Lambda/liquido/Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+    private int salario;
+
+    public Funcionario(int salario) {
+        this.salario = salario;
+    }
+
+    public Double getSalario() {
+        return (double) salario;
+    }
+}
